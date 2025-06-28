46a2059ba8decec3f9581be4b5cebf412428941d
Derek Elkins <derek.a.elkins@gmail.com>
2025-06-27 22:53:43 -0700

    Remove temp dir nonsense. Make cabal check happy.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for ipgcc
+
+## 0.1.0.0 -- 2025-06-27
+
+* First version.
--- test/NodeTests.hs
+++ test/NodeTests.hs
@@ -1,9 +1,9 @@
 module Main ( main ) where
-import Control.Exception ( catch, throwIO ) -- base
 import qualified Data.ByteString.Lazy.Char8 as LBS -- bytestring
-import System.Directory ( createDirectoryIfMissing, removeDirectoryRecursive ) -- directory
+import Data.Maybe ( fromMaybe ) -- base
+import System.Directory ( createDirectoryIfMissing, getTemporaryDirectory, removeFile ) -- directory
 import System.FilePath ( replaceExtension, takeBaseName ) -- filepath
-import System.Exit ( ExitCode ) -- base
+import System.Environment ( lookupEnv ) -- base
 import System.IO ( hClose, openBinaryTempFile ) -- base
 import System.Process ( readProcess ) -- process
 
@@ -13,49 +13,50 @@ import Test.Tasty.Golden ( findByExtension, goldenVsString ) -- tasty-golden
 import Text.IPG.Simple ( parseFile )
 import Text.IPG.Export.JS ( Context(..), defaultContext, toJSWithContext )
 
--- TODO: Make these configurable
-tMP_DIR :: FilePath
-tMP_DIR = "./tmp/"
-
 nODE_EXE :: String
 nODE_EXE = "node"
 
+type Env = (FilePath, String)
+
 main :: IO ()
 main = do
-    createDirectoryIfMissing True tMP_DIR
-    tests <- goldenTests
+    tmpDir <- getTemporaryDirectory
+    nodeExe <- fromMaybe nODE_EXE <$> lookupEnv "NODE_EXE"
+    createDirectoryIfMissing True tmpDir
+    tests <- goldenTests (tmpDir, nodeExe)
     defaultMain tests
-     `catch` (\e -> do
-        removeDirectoryRecursive tMP_DIR
-        throwIO (e :: ExitCode))
 
-run :: Bool -> FilePath -> IO LBS.ByteString
-run doDebugging f = do
-    results <- parseFile True f
-    case results of
+run :: Env -> Bool -> FilePath -> IO LBS.ByteString
+run (tmpDir, nodeExe) doDebugging f = do
+    parseResult <- parseFile True f
+    case parseResult of
         Left err -> return $ LBS.pack (show err)
         Right (preamble, g, _, postamble) -> do
-            (tmpFile, h) <- openBinaryTempFile tMP_DIR "test.js"
+            (tmpFile, h) <- openBinaryTempFile tmpDir "test.js"
             LBS.hPutStrLn h preamble
             LBS.hPutStrLn h (toJSWithContext (defaultContext { debugMode = doDebugging }) g)
             LBS.hPutStrLn h postamble
             hClose h
 
             -- TODO: There's surely a ByteString version of this.
-            LBS.pack <$> readProcess nODE_EXE [tmpFile] ""
+            result <- LBS.pack <$> readProcess nodeExe [tmpFile] ""
+
+            removeFile tmpFile
+
+            return result
 
 -- TODO: Have tests that compare interpreter and node output.
-goldenTests :: IO TestTree
-goldenTests = do
+goldenTests :: Env -> IO TestTree
+goldenTests env = do
     ipgFiles <- findByExtension [".ipg"] "test/node/"
     return $ testGroup "Node tests"
         [ testGroup "Non-Debugging"
-              [ goldenVsString (takeBaseName ipgFile) goldFile (run False ipgFile)
+              [ goldenVsString (takeBaseName ipgFile) goldFile (run env False ipgFile)
               | ipgFile <- ipgFiles
               , let goldFile = replaceExtension ipgFile ".release.json"
               ]
         , testGroup "Debugging"
-              [ goldenVsString (takeBaseName ipgFile) goldFile (run True ipgFile)
+              [ goldenVsString (takeBaseName ipgFile) goldFile (run env True ipgFile)
               | ipgFile <- ipgFiles
               , let goldFile = replaceExtension ipgFile ".debug.json"
               ]
