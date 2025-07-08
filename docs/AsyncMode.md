ef8fc1e2fb1a8d43303f91c1491cc8754fbbaca7
Derek Elkins <derek.a.elkins@gmail.com>
2025-07-08 13:00:48 -0700

    Mention issue with Firefox and AsyncMode example. Add QOI silliness.

--- docs/AsyncMode.md
+++ docs/AsyncMode.md
@@ -17,8 +17,19 @@ as needed. (In theory, I could exploit the parallelism in the data-dependency
 graph to run `slices` in parallel Haxl-style, but I've not implemented that.)
 
 The benefit of async mode is that fetching only the parts of the input
-that are necessary. For example, [docs/AsyncExample/ISO9660.html](docs/AsyncExample/ISO9660.html)
+that are necessary. For example, [docs/AsyncExample/viewer.html](AsyncExample/viewer.html)
 uses `Range` headers to select the relevant subset of the input in `fetch`
 requests. In practice, and as that example illustrates, you will want some kind of
 caching/prefetching layer so you're not making a bunch of requests for individual
 bytes.
+
+(As of writing this, Firefox won't work right on that page unless you tell it
+to only accept the `identity` encoding via `network.http.accept-encoding.secure`.
+I'm not sure if it is Firefox or GitHub that is doing the wrong thing here. I'm
+inclined to say the problem is with GitHub, though Firefox may also be doing something
+wrong. Basically, Firefox *adds* `identity` to the list of encodings in `Accept-Encoding`,
+but still has `gzip` (by default) in that list. GitHub's servers respond with
+a range response, but also `Content-Encoding: gzip`. My guess is that the `Content-Encoding`
+is completely spurious, since it's unclear what that would even mean for a range.
+It seems Chrome and Edge just don't send `Accept-Encoding` [which implies only `identity`]
+in this case forcing GitHub's hand.)
---
+++ docs/QOI.js
@@ -0,0 +1,579 @@
+function render(canvas, width, height, colors) {
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d");
+    const img = ctx.createImageData(width, height);
+    const data = img.data;
+    let i = 0;
+    for (let y = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x) {
+            const offset = 4 * i;
+            const color = colors[i++];
+            data[offset+0] = color.r;
+            data[offset+1] = color.g;
+            data[offset+2] = color.b;
+            data[offset+3] = color.a;
+        }
+    }
+    ctx.putImageData(img, 0, 0);
+}
+
+function initState() {
+    const black = { r: 0, g: 0, b: 0, a: 255 };
+    const history = new Array(64);
+    history.fill(black);
+    return { prevColor: black, history };
+}
+
+function concat(runs) {
+    return runs.flat();
+}
+
+function hash(color) {
+    return (color.r * 3 + color.g * 5 + color.b * 7 + color.a * 11) % 64;
+}
+
+function rgbCase(state, r, g, b) {
+    return rgbaCase(state, r, g, b, state.prevColor.a);
+}
+
+function rgbaCase(state, r, g, b, a) {
+    const color = { r, g, b, a };
+    state.prevColor = color;
+    state.history[hash(color)] = color;
+    return [color];
+}
+
+function indexCase(state, index) {
+    const color = state.history[index];
+    state.prevColor = color;
+    return [color];
+}
+
+function diffCase(state, dr, dg, db) {
+    return rgbCase(
+        state,
+        (state.prevColor.r + dr) & 0xFF,
+        (state.prevColor.g + dg) & 0xFF,
+        (state.prevColor.b + db) & 0xFF
+    );
+}
+
+function lumaCase(state, diffGreen, drdg, dbdg) {
+    return rgbCase(
+        state,
+        (state.prevColor.r + drdg + diffGreen) & 0xFF,
+        (state.prevColor.g + diffGreen) & 0xFF,
+        (state.prevColor.b + dbdg + diffGreen) & 0xFF
+    );
+}
+
+function runCase(state, runLength) {
+    const run = new Array(runLength);
+    run.fill(state.prevColor);
+    return run;
+}
+
+function _ipg_startsWith(s, l, r, prefix) {
+  if (r - l < prefix.length) return false;
+  if (typeof s === 'string') return s.startsWith(prefix, l);
+  for (let i = 0; i < prefix.length; ++i) {
+    if (s[l + i] !== prefix.charCodeAt(i)) return false;
+  }
+  return true;
+}
+function QOI(input, begin = 0, end = input.length) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    let nt_QOIHeader_0;
+    let nt_QOIChunks_0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // QOIHeader@0[0, EOI]
+    left = 0;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_QOIHeader_0 = QOIHeader(input, begin + left, begin + right);
+    if (nt_QOIHeader_0 === null) break _ipg_alt;
+    if (nt_QOIHeader_0._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_QOIHeader_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_QOIHeader_0._ipg_end);
+    }
+    nt_QOIHeader_0._ipg_end += left;
+    nt_QOIHeader_0._ipg_start += left;
+    left = nt_QOIHeader_0._ipg_start;
+    right = nt_QOIHeader_0._ipg_end;
+
+    // { width = QOIHeader@0.width }
+    self.width = nt_QOIHeader_0.width;
+
+    // { height = QOIHeader@0.height }
+    self.height = nt_QOIHeader_0.height;
+
+    // { channels = QOIHeader@0.channels }
+    self.channels = nt_QOIHeader_0.channels;
+
+    // { colorspace = QOIHeader@0.colorspace }
+    self.colorspace = nt_QOIHeader_0.colorspace;
+
+    // QOIChunks@0(initState())[QOIHeader@0.END, EOI]
+    left = nt_QOIHeader_0._ipg_end;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_QOIChunks_0 = QOIChunks(input, begin + left, begin + right, initState());
+    if (nt_QOIChunks_0 === null) break _ipg_alt;
+    if (nt_QOIChunks_0._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_QOIChunks_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_QOIChunks_0._ipg_end);
+    }
+    nt_QOIChunks_0._ipg_end += left;
+    nt_QOIChunks_0._ipg_start += left;
+    left = nt_QOIChunks_0._ipg_start;
+    right = nt_QOIChunks_0._ipg_end;
+
+    // { colors = concat(QOIChunks@0.values) }
+    self.colors = concat(nt_QOIChunks_0.values);
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function QOIHeader(input, begin = 0, end = input.length) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    let nt_BE_U32_0;
+    let nt_BE_U32_1;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // "qoif"[0, 4]
+    left = 0;
+    right = 4;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    if (!_ipg_startsWith(input, begin + left, begin + right, "qoif")) break _ipg_alt;
+    self._ipg_start = Math.min(self._ipg_start, left);
+    right = left + 4;
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // BE_U32@0[4, EOI]
+    left = 4;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_BE_U32_0 = BE_U32(input, begin + left, begin + right);
+    if (nt_BE_U32_0 === null) break _ipg_alt;
+    if (nt_BE_U32_0._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_BE_U32_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_BE_U32_0._ipg_end);
+    }
+    nt_BE_U32_0._ipg_end += left;
+    nt_BE_U32_0._ipg_start += left;
+    left = nt_BE_U32_0._ipg_start;
+    right = nt_BE_U32_0._ipg_end;
+
+    // { width = BE_U32@0.value }
+    self.width = nt_BE_U32_0.value;
+
+    // BE_U32@1[BE_U32@0.END, EOI]
+    left = nt_BE_U32_0._ipg_end;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_BE_U32_1 = BE_U32(input, begin + left, begin + right);
+    if (nt_BE_U32_1 === null) break _ipg_alt;
+    if (nt_BE_U32_1._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_BE_U32_1._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_BE_U32_1._ipg_end);
+    }
+    nt_BE_U32_1._ipg_end += left;
+    nt_BE_U32_1._ipg_start += left;
+    left = nt_BE_U32_1._ipg_start;
+    right = nt_BE_U32_1._ipg_end;
+
+    // { height = BE_U32@1.value }
+    self.height = nt_BE_U32_1.value;
+
+    // { channels = .[BE_U32@1.END] }
+    left = nt_BE_U32_1._ipg_end;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.channels = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { colorspace = .[BE_U32@1.END + 1] }
+    left = nt_BE_U32_1._ipg_end + 1;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.colorspace = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function QOIChunks(input, begin = 0, end = input.length, a_state) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    let nt_QOIChunk_0;
+    let nt_EndMarker_0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // repeat QOIChunk@0(state)[QOIChunk@0.END, EOI].chunk starting on [0, EOI] until EndMarker@0
+    left = 0;
+    right = EOI;
+    self.values = [];
+    while (true) {
+      if (left < 0 || right < left || right > EOI) break _ipg_alt;
+      nt_EndMarker_0 = EndMarker(input, begin + left, begin + right);
+      if (nt_EndMarker_0 !== null) {
+        if (nt_EndMarker_0._ipg_end !== 0) {
+          self._ipg_start = Math.min(self._ipg_start, left + nt_EndMarker_0._ipg_start);
+          self._ipg_end = Math.max(self._ipg_end, left + nt_EndMarker_0._ipg_end);
+        }
+        nt_EndMarker_0._ipg_end += left;
+        nt_EndMarker_0._ipg_start += left;
+        right = nt_EndMarker_0._ipg_end;
+        break;
+      }
+      nt_QOIChunk_0 = QOIChunk(input, begin + left, begin + right, a_state);
+      if (nt_QOIChunk_0 === null) break _ipg_alt;
+      if (nt_QOIChunk_0._ipg_end === 0) throw 'repeat of non-consuming rule: QOIChunk';
+      self._ipg_start = Math.min(self._ipg_start, left + nt_QOIChunk_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_QOIChunk_0._ipg_end);
+      nt_QOIChunk_0._ipg_end += left;
+      nt_QOIChunk_0._ipg_start += left;
+      self.values.push(nt_QOIChunk_0.chunk);
+      left = nt_QOIChunk_0._ipg_end;
+      right = EOI;
+    }
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function QOIChunk(input, begin = 0, end = input.length, a_state) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    let nt_Chunk_0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // { tagByte = .[0] }
+    left = 0;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.tagByte = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // Chunk@0(state, tagByte)[1, EOI]
+    left = 1;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_Chunk_0 = Chunk(input, begin + left, begin + right, a_state, self.tagByte);
+    if (nt_Chunk_0 === null) break _ipg_alt;
+    if (nt_Chunk_0._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_Chunk_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_Chunk_0._ipg_end);
+    }
+    nt_Chunk_0._ipg_end += left;
+    nt_Chunk_0._ipg_start += left;
+    left = nt_Chunk_0._ipg_start;
+    right = nt_Chunk_0._ipg_end;
+
+    // { chunk = Chunk@0.run }
+    self.chunk = nt_Chunk_0.run;
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function Chunk(input, begin = 0, end = input.length, a_state, a_tagByte) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte == 254 ]
+    if (!(a_tagByte == 254)) break _ipg_alt;
+
+    // { tag = "rgb" }
+    self.tag = "rgb";
+
+    // { r = .[0] }
+    left = 0;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.r = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { g = .[1] }
+    left = 1;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.g = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { b = .[2] }
+    left = 2;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.b = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { run = rgbCase(state, r, g, b) }
+    self.run = rgbCase(a_state, self.r, self.g, self.b);
+
+    return self;
+  }
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte == 255 ]
+    if (!(a_tagByte == 255)) break _ipg_alt;
+
+    // { tag = "rgba" }
+    self.tag = "rgba";
+
+    // { r = .[0] }
+    left = 0;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.r = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { g = .[1] }
+    left = 1;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.g = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { b = .[2] }
+    left = 2;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.b = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { a = .[3] }
+    left = 3;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.a = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    // { run = rgbaCase(state, r, g, b, a) }
+    self.run = rgbaCase(a_state, self.r, self.g, self.b, self.a);
+
+    return self;
+  }
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte >> 6 == 0 ]
+    if (!(a_tagByte >> 6 == 0)) break _ipg_alt;
+
+    // { tag = "index" }
+    self.tag = "index";
+
+    // { index = tagByte & 63 }
+    self.index = a_tagByte & 63;
+
+    // { run = indexCase(state, index) }
+    self.run = indexCase(a_state, self.index);
+
+    return self;
+  }
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte >> 6 == 1 ]
+    if (!(a_tagByte >> 6 == 1)) break _ipg_alt;
+
+    // { tag = "diff" }
+    self.tag = "diff";
+
+    // { dr = (tagByte >> 4 & 3) - 2 }
+    self.dr = (a_tagByte >> 4 & 3) - 2;
+
+    // { dg = (tagByte >> 2 & 3) - 2 }
+    self.dg = (a_tagByte >> 2 & 3) - 2;
+
+    // { db = (tagByte & 3) - 2 }
+    self.db = (a_tagByte & 3) - 2;
+
+    // { run = diffCase(state, dr, dg, db) }
+    self.run = diffCase(a_state, self.dr, self.dg, self.db);
+
+    return self;
+  }
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    let nt_U8_0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte >> 6 == 2 ]
+    if (!(a_tagByte >> 6 == 2)) break _ipg_alt;
+
+    // { tag = "luma" }
+    self.tag = "luma";
+
+    // { diffGreen = (tagByte & 63) - 32 }
+    self.diffGreen = (a_tagByte & 63) - 32;
+
+    // U8@0[0, EOI]
+    left = 0;
+    right = EOI;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    nt_U8_0 = U8(input, begin + left, begin + right);
+    if (nt_U8_0 === null) break _ipg_alt;
+    if (nt_U8_0._ipg_end !== 0) {
+      self._ipg_start = Math.min(self._ipg_start, left + nt_U8_0._ipg_start);
+      self._ipg_end = Math.max(self._ipg_end, left + nt_U8_0._ipg_end);
+    }
+    nt_U8_0._ipg_end += left;
+    nt_U8_0._ipg_start += left;
+    left = nt_U8_0._ipg_start;
+    right = nt_U8_0._ipg_end;
+
+    // { drdg = (U8@0.value >> 4) - 8 }
+    self.drdg = (nt_U8_0.value >> 4) - 8;
+
+    // { dbdg = (U8@0.value & 15) - 8 }
+    self.dbdg = (nt_U8_0.value & 15) - 8;
+
+    // { run = lumaCase(state, diffGreen, drdg, dbdg) }
+    self.run = lumaCase(a_state, self.diffGreen, self.drdg, self.dbdg);
+
+    return self;
+  }
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // ?[ tagByte >> 6 == 3 ]
+    if (!(a_tagByte >> 6 == 3)) break _ipg_alt;
+
+    // { tag = "run" }
+    self.tag = "run";
+
+    // { runLength = (tagByte & 63) + 1 }
+    self.runLength = (a_tagByte & 63) + 1;
+
+    // { run = runCase(state, runLength) }
+    self.run = runCase(a_state, self.runLength);
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function EndMarker(input, begin = 0, end = input.length) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // "\x00\x00\x00\x00\x00\x00\x00\x01"[0, 8]
+    left = 0;
+    right = 8;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    if (!_ipg_startsWith(input, begin + left, begin + right, "\x00\x00\x00\x00\x00\x00\x00\x01")) break _ipg_alt;
+    self._ipg_start = Math.min(self._ipg_start, left);
+    right = left + 8;
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function U8(input, begin = 0, end = input.length) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // { value = .[0] }
+    left = 0;
+    right = left + 1;
+    if (left < 0 || right > EOI) break _ipg_alt;
+    self.value = input[begin + left];
+    self._ipg_start = Math.min(self._ipg_start, left);
+    self._ipg_end = Math.max(self._ipg_end, right);
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+function BE_U32(input, begin = 0, end = input.length) {
+  const EOI = end - begin; let self;
+  
+  _ipg_alt: {
+    let left = EOI; let right = 0; let loopEnd = 0;
+    self = { _ipg_start: EOI, _ipg_end: 0 };
+
+    // { bs = *[0, 4] }
+    left = 0;
+    right = 4;
+    if (left < 0 || right < left || right > EOI) break _ipg_alt;
+    self.bs = input.slice(begin + left, begin + right);
+    if (left !== right) {
+      self._ipg_start = Math.min(self._ipg_start, left);
+      self._ipg_end = Math.max(self._ipg_end, right);
+    }
+
+    // { value = bs[3] | bs[2] << 8 | bs[1] << 16 | bs[0] << 24 }
+    self.value = self.bs[3] | self.bs[2] << 8 | self.bs[1] << 16 | self.bs[0] << 24;
+
+    return self;
+  }
+
+  
+  return null;
+}
+
+export async function drawQOI(canvas, src) {
+    const response = await fetch(src);
+    const qoiData = QOI(await response.bytes());
+    render(canvas, qoiData.width, qoiData.height, qoiData.colors); 
+}
